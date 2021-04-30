855c04e713086f1a45f02be8f268f02ca808af45
Dongmao Zhang <deanraccoon@gmail.com>
2021-04-29 21:00:59 -0700

    erasure code: change to online mode

--- erasure_code/codec.go
+++ erasure_code/codec.go
@@ -5,6 +5,7 @@ import (
 )
 
 type ErasureCodec interface {
-	Encode(input io.Reader, dataShards int, parityShards int, output []io.Writer, chunkSize int, fSize int64) error
+	Encode(input []byte, dataShards int, parityShards int, chunkSize int) ([][]byte, error)
+	Decode(input [][]byte, dataShards uint32, parityShards uint32, chunkSize int) ([]byte, error)
 	Reconstruct(input []io.Reader, dataShards int, parityShards int,  output []io.Writer, chunkSize int) error
 }
--- erasure_code/rs_codec_test.go
+++ erasure_code/rs_codec_test.go
@@ -1,56 +1,33 @@
 package erasure_code
 
 import (
-	"bytes"
-	"io"
 	"testing"
 
 	"github.com/journeymidnight/autumn/utils"
+	"github.com/journeymidnight/autumn/xlog"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
 )
 
+
+func init() {
+	xlog.InitLog([]string{"test.log"}, zap.DebugLevel)
+}
 
 func TestRsCodecs(t *testing.T) {
 	data := make([]byte, 4 << 10)
 	utils.SetRandStringBytes(data)
-	//test 4+3
-	input := bytes.NewBuffer(data)
-	output := make([]io.Writer, 7)
-	for i := range output {
-		output[i] = new(bytes.Buffer)
-	}
-		
-	//convert to io.Writer
-
-	err := RSEncoder{}.Encode(input, 4, 3, output, 1<<10, 4 << 10)
-
+	//test 6+3
+	output, err := RSEncoder{}.Encode(data, 6, 3, 4<<10)
 	require.Nil(t, err)
-	require.Equal(t,data[:1<<10], output[0].(*bytes.Buffer).Bytes())
+	require.Equal(t, 9,len(output))
 
-	//reconstruct
-	currupt := make([]io.Reader, len(output))
-	for i := range currupt {
-		if i == 0 || i == 3{
-			currupt[i] = nil
-			continue
-		}
-		currupt[i] = bytes.NewBuffer(output[i].(*bytes.Buffer).Bytes())
-	}
-
-
-	
-	fixed := make([]io.Writer, 7)
-	for i := range fixed {
-		if i == 0 || i == 3{
-			fixed[i] = new(bytes.Buffer)
-			continue
-		}
-		fixed[i] = nil
-	}
-
-	err = RSEncoder{}.Reconstruct(currupt, 4, 3, fixed, 1 << 10, 1 << 10)
+	output[0] = nil
+	output[5] = nil
+	output[8] = nil
+	haha, err := RSEncoder{}.Decode(output, 6, 3, 4 << 10)
 	require.Nil(t, err)
-	require.Equal(t, data[:1<<10], fixed[0].(*bytes.Buffer).Bytes())
-	require.Equal(t, data[3<<10:], fixed[3].(*bytes.Buffer).Bytes())
+	require.Equal(t,  len(data), len(haha))
+	require.Equal(t, data, haha)
 
 }
