822f6912bcc33729ab27d14f90a9e64b7b023676
Dongmao Zhang <deanraccoon@gmail.com>
2021-04-29 21:00:59 -0700

    erasure code: change to online mode

--- erasure_code/rs_codec.go
+++ erasure_code/rs_codec.go
@@ -1,13 +1,11 @@
 package erasure_code
 
 import (
-	"bytes"
+	"encoding/binary"
 	"io"
-	"math"
 
 	"github.com/journeymidnight/autumn/utils"
 	"github.com/klauspost/reedsolomon"
-	"github.com/pkg/errors"
 )
 
 type RSEncoder struct{} 
@@ -35,80 +33,77 @@ func (RSEncoder ) Reconstruct(input []io.Reader, dataShards int, parityShards in
 	return nil
 }
 
+const (
+	metaSize = 4
+)
+func (RSEncoder) Decode(input [][]byte, dataShards uint32, parityShards uint32, cellSize uint32) ([]byte, error) {
 
-type Cached struct {
-	io.ReadWriter
-	wb *bytes.Buffer
-	rb *bytes.Buffer
-}
-
-func NewCached() *Cached {
-	return &Cached{
-		wb: new(bytes.Buffer),
-		rb:nil,
-	}
-}
-func (c *Cached) Write(p []byte) (n int, err error) {
-	return c.wb.Write(p)
-}
-
-func (c *Cached) ResetForRead() {
-	c.rb = bytes.NewBuffer(c.wb.Bytes())
-	return
-}
-
-func (c *Cached) Read(p []byte) (n int, err error) {
-	return c.rb.Read(p)
-}
-
-
-
-func (RSEncoder) Encode(input io.Reader, dataShards int, parityShards int, output []io.Writer, chunkSize int, fSize int64) error {
-	if len(output) != dataShards + parityShards {
-		return errors.Errorf("%d+%d != len(output) [%d]", dataShards, parityShards, len(output))
-	}
-	
-	enc, err := reedsolomon.NewStream(dataShards, parityShards)
+	enc, err := reedsolomon.New(int(dataShards), int(parityShards))
 	if err != nil {
-		return err
+		return nil, err
 	}
-	groupSize := int64(dataShards * chunkSize)
 
-
-	utils.AssertTrue(fSize <= math.MaxUint32)
-	utils.AssertTrue(chunkSize & (chunkSize - 1)==0)
-
-
-	for i := int64(0); i < fSize ; i+= int64(groupSize) {
-
-		r := io.LimitReader(input, int64(groupSize))
-
-		cached := make([]*Cached, dataShards, dataShards)
-		
-		for i := range cached {
-			cached[i] = NewCached() 
-		}
-
-		multiWrites := make([]io.Writer, dataShards, dataShards)
-		for i := range output[:dataShards] {
-			multiWrites[i] = io.MultiWriter(cached[i], output[i])
-		}
-		enc.Split(r, multiWrites, groupSize) //write dataShards
-
-
-		for i := range cached {
-			cached[i].ResetForRead()
-		}
-
-		
-		rCache := make([]io.Reader, len(cached))
-		for i := range rCache {
-			rCache[i] = cached[i]
-		}
-		err = enc.Encode(rCache, output[dataShards:])
-		if err != nil {
-			return err
-		}
+	// Verify the shards
+	ok, err := enc.Verify(input)
+	if !ok {
+		err = enc.Reconstruct(input)
+			if err != nil {
+				return nil, err
+			}
 	}
-	return nil
+
+	//join data
+	dataLength := binary.BigEndian.Uint32(input[0][:metaSize])
+	fullData := make([]byte, dataLength + metaSize)
+	actualSize := len(fullData)
+	cellNums := utils.Ceil(uint32(actualSize), uint32(cellSize)) / uint32(cellSize)
+	for k := uint32(0) ; k <  cellNums ; k ++ {
+		i := k / dataShards //row number of input
+		j := k % dataShards //column number of input
+		n := copy(fullData[(k*cellSize):], input[j][i*cellSize:])
+		utils.AssertTrue(n > 0)
+	}
+	return fullData[metaSize:], nil
 }
+
+func (RSEncoder) Encode(input []byte, dataShards uint32, parityShards uint32, cellSize uint32) ([][]byte, error) {
+
+	enc, err := reedsolomon.New(int(dataShards), int(parityShards))
+	if err != nil {
+		return nil, err
+	}
+	rawSize := uint32(len(input))
+	actualSize := rawSize + metaSize
+	groupSize := int64(dataShards * cellSize)
+	//groupSize is not power of 2, can not use utils.Ceil
+	clusterSize := (actualSize + uint32(groupSize) - 1) / uint32(groupSize) * uint32(groupSize)
+	objectSize := clusterSize / dataShards
+
+	data := make([][]byte, dataShards + parityShards)
+	for i := uint32(0) ; i < dataShards + parityShards ; i++ {
+		data[i] = make([]byte, objectSize)
+	}
+
+	//fill the first cell
+	binary.BigEndian.PutUint32(data[0], uint32(rawSize))
+	copy(data[0][metaSize:], input[0:cellSize-metaSize])
+
+	//fill the other cell
+	cellNums := utils.Ceil(uint32(actualSize), uint32(cellSize)) / uint32(cellSize)
+	for k := uint32(1) ; k <  cellNums ; k ++ {
+		i := k / dataShards
+		j := k % dataShards
+
+		var availData uint32
+		if cellSize < actualSize - k*cellSize {
+			availData = cellSize
+		} else {
+			availData = actualSize - k*cellSize
+		}
+		copy(data[j][i*cellSize:], input[k*cellSize-metaSize:k*cellSize-metaSize+availData])
+	}
+
+	err = enc.Encode(data)
+	return data, err
+}
+
