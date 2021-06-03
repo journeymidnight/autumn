c564fd4dbef6a5e1dbeea4bc6af059a75a03322e
Dongmao Zhang <deanraccoon@gmail.com>
2021-06-02 20:15:19 -0700

    Add assert to make sure rp.tables is sorted

--- partitionserver/ps.go
+++ partitionserver/ps.go
@@ -120,7 +120,7 @@ func (ps *PartitionServer) startRangePartition(meta *pspb.PartitionMeta) error {
 	utils.AssertTrue(meta.PartID != 0)
 
 	rp := range_partition.OpenRangePartition(meta.PartID, row, log, ps.blockReader, meta.Rg.StartKey, meta.Rg.EndKey, locs,
-		blobs, ps.pmClient, openStream)
+		blobs, ps.pmClient, openStream, range_partition.DefaultOption())
 
 	//FIXME: check each partID is uniq
 	ps.Lock()
