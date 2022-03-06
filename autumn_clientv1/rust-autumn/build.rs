cf5200ca04c108c868e881ac0116b7782d43415e
Dongmao Zhang <deanraccoon@gmail.com>
2022-03-06 12:24:09 -0800

    fix(rust-autumn) : build pspb.proto and pb.proto

--- autumn_clientv1/rust-autumn/build.rs
+++ autumn_clientv1/rust-autumn/build.rs
@@ -1,8 +1,31 @@
+use std::path::Path;
+use git2::{ErrorCode, Repository};
+
+
+
 fn main() {
+    match Repository::clone(
+        "https://github.com/googleapis/googleapis.git",
+        Path::new("googleapis"),
+    ) {
+        Ok(_) => println!("Successfully cloned googleapis"),
+        Err(e) => match e.code() {
+            ErrorCode::Exists => {
+                println!("Repository already exists, skipping clone")
+            }
+            _ => panic!("Error: {}", e),
+        },
+    }
     let proto_root = "../../proto";
     tonic_build::configure()
         .build_server(false)
         .build_client(true)
         .out_dir("src")
-        .compile(&["pspb.proto"], &[proto_root]).expect("Failed to compile protos");
+        .compile(&["pspb.proto"], &[proto_root, "googleapis"]).expect("Failed to compile protos");
+    
+    tonic_build::configure()
+        .build_server(false)
+        .build_client(true)
+        .out_dir("src")
+        .compile(&["pb.proto"], &[proto_root, "googleapis"]).expect("Failed to compile protos");
 }
